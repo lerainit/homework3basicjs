1a444c05f936719dfee3ac2eb9d033c91352f266
Valeria Vitvytska <leranmu@gmail.com>
2022-04-06 21:06:41 +0300

    Update 3.js

--- 3.js
+++ 3.js
@@ -8,15 +8,15 @@ let userNumber = +prompt( 'Enter your number here!' );
   while ( userNumber % 1 !== 0 || isNaN(userNumber) || userNumber === 0 ){
 	
 
-     userNumber = +prompt( 'Enter your number here!' );
+         userNumber = +prompt( 'Enter your number here!' );
 
-}
+  }
 
 
 if ( userNumber < 5 ){
 
 
-   console.log('Sorry,no number');
+    console.log('Sorry,no number');
 
 
 }
@@ -25,9 +25,9 @@ else {
 
    for ( let i = 0; i <= userNumber; i = i + 5 ){
 
-       console.log(i);
+        console.log(i);
 
-     }
+   }
 
 }
 
