459ee523e71a92c2b738b7e40e0a3b6243d0c2e4
Valeria Vitvytska <leranmu@gmail.com>
2022-04-06 18:22:33 +0300

    Update 3.js

--- 3.js
+++ 3.js
@@ -33,32 +33,3 @@ console.log(i);
 
 }
 
-/*----------------------------------------------------------------------*/
-
-
-let m = +prompt('Введите первое число!')
-
-
-let n = +prompt('Введите второе число');
-
-nextPrime:
-
-for ( m ; m <= n; m++) { 
-
-
-for (let j = 2; j < m; j++) {
-
-
-if (m % j == 0) continue nextPrime; 
-
-}
-
-console.log( m ); 
-
-
-}
-
-
-
-
-
